543a4f58972f1c213656c1fa8ede237eba55e2ea
Chuanjo <chuanjo@hotmail.com>
2022-02-22 21:13:55 +0100

    lab done

--- routes/movies.routes.js
+++ routes/movies.routes.js
@@ -37,6 +37,60 @@ router.get("/", (req,res,next)=>{
         next(err)
     })
 })
+router.get("/:id/details", (req,res,next)=>{
+    // const id = req.params.id  ==> es lo mismo que la linea siguiente
+    const {id} = req.params
+    MovieModel.findById(id)
+    .populate("cast")
+    .then((oneMovie)=>{
+      res.render("movies/movie-details.hbs", {oneMovie})
+    })
+    .catch((err)=>{
+      next(err)
+    })
+  })
+
+router.post("/:id/delete", async (req,res,next)=>{
+    try {
+        const {id}=req.params
+
+        await MovieModel.findByIdAndDelete(id)
+
+        res.redirect("/movies")
+    }
+    catch(err){
+        next(err)
+    }
+})
+
+router.get("/:id/edit",(req,res,next)=>{
+
+    const {id} = req.params
+    MovieModel.findById(id)
+    .then((oneMovieParam)=>{
+      oneMovie = oneMovieParam;
+      return CelebrityModel.find()
+    })
+    .then((allCelebrities)=>{
+      res.render("movies/edit-movie.hbs", {oneMovie, allCelebrities})
+    })
+    .catch((err)=>{
+        next(err)
+    })
+  })
+
+router.post("/:id/edit",(req,res,next)=>{
+    const { id } = req.params 
+    const {title,genre,plot,cast} = req.body
+    
+    MovieModel.findByIdAndUpdate(id, {title,genre,plot,cast})
+    .then((updatedMovie)=>{
+        res.redirect(`/movies/${updatedMovie._id}/edit`)
+    })
+    .catch((err)=>{
+        next(err)
+    })
+})
 
 
 
